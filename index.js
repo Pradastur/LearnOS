9707c43f3ea222c44d21d0072b8be5843250b479
Pradastur <pradastur@gmail.com>
2017-11-17 12:53:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ app.use(express.static('public'));
 app.get('/', function (req, res) {
  res.sendFile(__dirname+'/public/index.html');
 });
-app.listen(5000, function(){
-  console.log("app.js listening on port 5000")
-})
+app.listen(process.env.PORT || 3000,function(){
+  console.log('listening on',app.address().port);
+});
