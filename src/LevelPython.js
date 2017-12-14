34f2e5304144df0a54b0b66b51f3aa4401dc4f3d
Pradastur <pradastur@gmail.com>
2017-12-14 09:06:25 +0100

    Update LevelPython.js

--- src/LevelPython.js
+++ src/LevelPython.js
@@ -18,9 +18,9 @@ export class LevelPython extends React.Component{
 				</div>
 				<div className="listLevel">
 					<ul>
-						<li onClick={this.changeToLesson1}>Lesson 1</li>
-						<li onClick={this.changeToLesson2}>Lesson 2</li>
-						<li onClick={this.changeToLesson3}>Lesson 3</li>
+						<li onClick={this.props.changeToLesson1}>Lesson 1</li>
+						<li onClick={this.props.changeToLesson2}>Lesson 2</li>
+						<li onClick={this.props.changeToLesson3}>Lesson 3</li>
 					</ul>
 				</div>
 			</div>
